dd6f3e20272229fec4305213dfb0d23c8d0065c2
Asim <asim.awad98@gmail.com>
2025-05-08 01:52:15 +0200

    fixed openai api issue

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
-response = client.chat.completions.create(model="gpt-o4-mini",
+response = client.chat.completions.create(model="gpt-4o-mini",
 messages=[
     {"role": "system", "content": "You are a helpful assistant."},
     {"role": "user", "content": "what model are you!, like what version exactly?"},
